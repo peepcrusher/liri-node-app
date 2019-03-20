1950a7e936757a033cd70d25af2eaf62cd5947f4
Sam Thompson <electronic62794@yahoo.com>
2019-03-19 17:03:48 -0700

    movie-this function works

--- liri.js
+++ liri.js
@@ -2,9 +2,9 @@
 //require all necessary packages 
 var dotenv = require("dotenv").config();
 var keys = require("./keys.js");
-
-var Spotify = require("node-spotify-api")
-var axios = require("axios")
+var fs = require("fs");
+var Spotify = require("node-spotify-api");
+var axios = require("axios");
 var moment = require("moment");
 
 var spotify = new Spotify(keys.spotify);
@@ -15,29 +15,7 @@ var nodeArgs = process.argv
 //assigning the first user input as the action
 var action = process.argv[2];
 
-if(action === "concert-this"){
-
-}
-
-if(action === "spotify-this-song"){
-    
-}
-
-if(action === "movie-this"){
-    
-}
-
-if(action === "do-what-it-says"){
-    
-}
-
-else{
-    console.log("Please use one of these functions")
-    console.log("concert-this <artist name>")
-    console.log("spotify-this-song <song name>")
-    console.log("movie-this <movie name>")
-    console.log("do-what-it-says")
-}
+//variable for the subject of the search
 var subject = "";
 
 for (var i = 3; i < nodeArgs.length; i++){
@@ -49,5 +27,42 @@ for (var i = 3; i < nodeArgs.length; i++){
     
       }
 }
+
+if(action === "concert-this"){
+
+}
+
+if(action === "spotify-this-song"){
+    
+}
+
+if(action === "movie-this"){
+    var queryUrl = "http://www.omdbapi.com/?t=" + subject + "&y=&plot=short&apikey=trilogy"
+
+axios.get(queryUrl)
+.then(function(response){
+    console.log("Title: " + response.data.Title);
+    console.log("Release Year: " + response.data.Year);
+    console.log("IMDB Rating: " + response.data.imdbRating);
+    console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value)
+    console.log("Country of Origin: " + response.data.Country);
+    console.log("Movie Language: " + response.data.Language);
+    console.log("Plot: " + response.data.Plot);
+    console.log("Actors: " + response.data.Actors);
+})
+}
+
+if(action === "do-what-it-says"){
+    
+}
+
+// else{
+//     console.log("Please use one of these functions")
+//     console.log("concert-this <artist name>")
+//     console.log("spotify-this-song <song name>")
+//     console.log("movie-this <movie name>")
+//     console.log("do-what-it-says")
+// }
+
 
 
